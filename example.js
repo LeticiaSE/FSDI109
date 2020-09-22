d3316739cafe313f87f73f4f812c286aaf3a670e
LeticiaSE <35822181+LeticiaSE@users.noreply.github.com>
2020-09-22 04:47:13 -0700

    Harmony

---
+++ example.js
@@ -0,0 +1,85 @@
+<ion-Header>
+    <ion-toolbar color="secondary">
+        <ion-title>
+            Feed
+        </ion-title>
+    </ion-toolbar>
+</ion-Header>
+
+<ion-content color="light" [fullscreen]="true">
+
+<!--
+
+    <ul>
+        <li> *ngFor="let p of postToDisplay" </li></l>
+-->
+<ion>
+</ion>
+
+
+ion-card-header{
+    padding-top: 5px;
+}
+
+ion-card-
+
+
+<ion-card-header>
+
+</ion-card-header>
+
+ion-card{
+    margin-bottom: 12px;
+    margin-top:12px;
+    boder-radious:5px;
+    
+    img{
+        
+    }
+}
+
+
+import { firestore } from 'firebase';
+
+this.DataCue.getAllPost observable
+this.DataCue.getAllPost().subscribe(list
+//package.Timestamp = new firestore.Timestamp(wrongFormat.seconds, wrongFormat.nanoseconds).toDate();
+p.timeStamp = new firestore.Timestamp(wrongFormat.seconds, wrongFormat.nanoseconds).toDate();
+
+
+
+return p;
+});
+
+console.log(this.postToDisplay);
+});
+}
+}
+
+
+//short the array of posts
+this.postToDisplay = this.postToDisplay.short(function(a, b)){
+    if(a.timeStamp > b.timeStamp){
+        return -1;
+    }
+    else{
+        return 1;// they are fine, leave them in that order (a -> b)
+    
+    }
+});
+
+
+
+
+
+38
+20
+14
+
+72
+
+-12
+-6
+
+54
+
